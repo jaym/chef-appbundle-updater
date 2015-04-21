b21cb708c3a3b1f23ef752ae3eb97ce035b6fc8a
Jay Mundrawala <jdmundrawala@gmail.com>
2015-04-21 15:29:49 -0700

    Add attributes to select the org, repo, and refname

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,10 +6,10 @@ end
 include_recipe 'build-essential::default'
 
 github_repo 'chef/chef' do
-  owner   'chef'
-  repo    'chef'
-  refname 'master'
-  destination "#{Chef::Config[:file_cache_path]}/chef"
+  owner       node['chef_appbundle_updater']['github_org']
+  repo        node['chef_appbundle_updater']['github_repo']
+  refname     node['chef_appbundle_updater']['refname']
+  destination node['chef_appbundle_updater']['destination']
 end
 
-appbundle "#{Chef::Config[:file_cache_path]}/chef"
+appbundle node['chef_appbundle_updater']['destination']
