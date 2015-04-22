3ca7a854f0fa7c17ee138b11ee599b6d0753ba16
Jay Mundrawala <jdmundrawala@gmail.com>
2015-04-21 19:12:01 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jdmundrawala@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures chef-appbundle-updater'
 long_description 'Installs/Configures chef-appbundle-updater'
-version          '0.0.1'
+version          '0.0.2'
 
 depends 'build-essential', '~> 2.2.3'
 depends 'apt', '~> 2.7.0'
